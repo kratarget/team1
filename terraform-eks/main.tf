82a97fd7678e73e62f1517cc28e0cd34e4827e33
Wim Van den Wyngaert <wim.vandenwyngaert@engie.com>
2025-02-06 18:55:09 +0100

    delete dummy parameter

--- terraform-eks/main.tf
+++ terraform-eks/main.tf
@@ -1,5 +1,5 @@
-resource "aws_ssm_parameter" "dummy-param" {
-  name  = "/hackathon/dummy-param"
-  type  = "String"
-  value = "dummy"
-}
+# resource "aws_ssm_parameter" "dummy-param" {
+#   name  = "/hackathon/dummy-param"
+#   type  = "String"
+#   value = "dummy"
+# }
