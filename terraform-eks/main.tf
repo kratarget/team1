9ae24aea3f20c9f10fffa596b75812d704509b70
Wim Van den Wyngaert <wim.vandenwyngaert@engie.com>
2025-02-06 13:29:01 +0100

    update gitignore

---
+++ terraform-eks/main.tf
@@ -0,0 +1,20 @@
+resource "aws_ssm_parameter" "dummy-team1" {
+  name  = "/hackathon/dummyteam1"
+  type  = "String"
+  value = "Hello"
+}
+#
+# module "terraform-aws-eks" {
+#   source = "./terraform-aws-eks"
+#   providers = {
+#     aws = aws
+#   }
+#
+#   app_name = "team1-terraform"
+#   environment = "development"
+#   cluster_version = "1.31"
+#   subnet_ids = [
+#     "subnet-05f81c1043c2d4852",
+#     "subnet-03cca5bfbe0945398"
+#   ]
+# }
