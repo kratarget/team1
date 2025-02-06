c9bfdc96f6bda98116927d27680f0e9734bec082
Jurre van der Gaag - Terpstra <jurre@terpstra.tech>
2025-02-06 15:10:01 +0100

    second EKS cluster

--- terraform-eks/main.tf
+++ terraform-eks/main.tf
@@ -12,3 +12,18 @@ module "terraform-aws-eks" {
     "subnet-0ad66248d4ac0abb3"
   ]
 }
+
+module "terraform-aws-eks2" {
+  source = "./terraform-aws-eks"
+  providers = {
+    aws = aws
+  }
+
+  app_name        = "team1-terraform2"
+  environment     = "development"
+  cluster_version = "1.31"
+  subnet_ids = [
+    "subnet-02d7fe499e716ba1c",
+    "subnet-0ad66248d4ac0abb3"
+  ]
+}
