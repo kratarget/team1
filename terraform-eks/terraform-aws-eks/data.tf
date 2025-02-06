9e8cfb69e8ac864c1ec010264fce3c418d6eef39
Jurre van der Gaag - Terpstra <jurre@Jurres-MacBook-Pro.local>
2025-02-06 14:02:39 +0100

    change roles names

--- terraform-eks/terraform-aws-eks/data.tf
+++ terraform-eks/terraform-aws-eks/data.tf
@@ -1,10 +1,9 @@
 data "aws_region" "current" {}
 
 data "aws_iam_role" "cluster" {
-  name = coalesce(var.cluster_role, "AmazonEKSAutoClusterRole-Default")
+  name = coalesce(var.cluster_role, "AWSServiceRoleForAmazonEKS")
 }
 
 data "aws_iam_role" "node" {
-  name = coalesce(var.node_role, "AmazonEKSAutoNodeRole-Default")
+  name = coalesce(var.node_role, "AWSServiceRoleForAmazonEKSNodegroup")
 }
-
