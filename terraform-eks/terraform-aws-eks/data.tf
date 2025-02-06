d427bd4e4a6cd934809204af7690ed639fc8a647
Wim Van den Wyngaert <wim.vandenwyngaert@engie.com>
2025-02-06 18:28:22 +0100

    cleanup current EKS cluster (will be rebuild on Friday)

--- terraform-eks/terraform-aws-eks/data.tf
+++
@@ -1,9 +0,0 @@
-data "aws_region" "current" {}
-
-data "aws_iam_role" "cluster" {
-  name = coalesce(var.cluster_role, "EKSClusterRole")
-}
-
-data "aws_iam_role" "node" {
-  name = coalesce(var.node_role, "AmazonEKSAutoNodeRole-Default")
-}
