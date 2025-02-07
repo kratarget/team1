018e68b176cc937af7e4f636f33bc5d4d9dd3e4c
JS5598-A <wajdi.yaich@external.engie.com>
2025-02-07 15:22:52 +0100

    add waf ressource

---
+++ terraform-waf/waf.tf
@@ -0,0 +1,59 @@
+resource "aws_wafv2_web_acl" "team1-waf-seahorse" {
+  name        = "team1-waf-seahorse-ruleset"
+  description = "Example WAF with managed rules"
+  scope       = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesSQLiRuleSet"
+    priority = 2
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesSQLiRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesSQLiRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "team1-waf-seahorse"
+    sampled_requests_enabled   = true
+  }
+}
