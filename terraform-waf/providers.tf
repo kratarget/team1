1d8461f66a230cd666608a162dc9592711155a39
JS5598-A <wajdi.yaich@external.engie.com>
2025-02-07 15:40:25 +0100

    add provider file to waf

---
+++ terraform-waf/providers.tf
@@ -0,0 +1,20 @@
+variable "AWS_REGION" { default = "eu-west-1" }
+variable "AWS_ACCESS_KEY" { default = null }
+variable "AWS_SECRET_KEY" { default = null }
+variable "AWS_PROFILE" { default = null }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.0.0,<6.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile    = var.AWS_PROFILE
+  region     = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+}
